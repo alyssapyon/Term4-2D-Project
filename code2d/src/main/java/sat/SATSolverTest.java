fff8e11fd0af8a692428f35135334cb07f8b325d
alyssapyon <50566448+alyssapyon@users.noreply.github.com>
2020-10-22 15:26:52 +0800

    updated to ead the filenames from the args array

--- code2d/src/main/java/sat/SATSolverTest.java
+++ code2d/src/main/java/sat/SATSolverTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 */
 
 import java.io.*;
+import java.nio.file.*;
 import java.util.*;
 import java.lang.*;
 
@@ -26,98 +27,104 @@ public class SATSolverTest {
     // TODO: add the main method that reads the .cnf file and calls SATSolver.solve to determine the satisfiability
     public static void main(String[] args) {
         ////////////////////////////////////////////////////////////////////////////////////////////
+        if (args.length == 0) {
+            System.out.println("ERROR: NO .CNF FILE PASSED IN");
+        } else {
+            // PART 1: CNF FILE TO FORMULA
 
-        // PART 1: CNF FILE TO FORMULA
+            try {
+                // open file with BufferedReader
+                // BufferedReader reader = new BufferedReader(new FileReader("sat1.cnf"));
+                // BufferedReader reader = new BufferedReader(new FileReader("test_2020.cnf"));
+                Path path = Paths.get(args[0]);
+                BufferedReader reader = new BufferedReader(new FileReader(args[0]));
 
-        try {
-            // open file with BufferedReader
-            // BufferedReader reader = new BufferedReader(new FileReader("sat1.cnf"));
-            BufferedReader reader = new BufferedReader(new FileReader("test_2020.cnf"));
+                // declare variables
+                // use ArrayList for easy insertion of each literal/clause
+                String newLine = null;
+                ArrayList<Clause> clauses = new ArrayList<Clause>();
+                ArrayList<Literal> literals = new ArrayList<Literal>();
 
-            // declare variables
-            // use ArrayList for easy insertion of each literal/clause
-            String newLine = null;
-            ArrayList<Clause> clauses = new ArrayList<Clause>();
-            ArrayList<Literal> literals = new ArrayList<Literal>();
+                // ensure the line is not empty
+                while ((newLine = reader.readLine()) != null) {
+                    // ignore the line if its a comment/problem line
+                    if (newLine.length() <= 0 || newLine.charAt(0) == 'p' || newLine.charAt(0) == 'c') {
+                        continue;
+                        // only continue if its a clause line
+                    } else {
+                        // remove newline and empty spaces
+                        // turn the line of String into an array
+                        newLine = newLine.trim();
+                        newLine = newLine.replace("\n", "");
+                        String[] newSplitLine = newLine.split("\\s+");
 
-            // ensure the line is not empty
-            while ((newLine = reader.readLine()) != null) {
-                // ignore the line if its a comment/problem line
-                if (newLine.length() <= 0 || newLine.charAt(0) == 'p' || newLine.charAt(0) == 'c') {
-                    continue;
-                    // only continue if its a clause line
-                } else {
-                    // remove newline and empty spaces
-                    // turn the line of String into an array
-                    newLine = newLine.trim();
-                    newLine = newLine.replace("\n", "");
-                    String[] newSplitLine = newLine.split("\\s+");
+                        // for each element in the array, create a matching literal to add into the clause
+                        for (String i : newSplitLine) {
+                            int numi = Integer.parseInt(i);
+                            // numi == 0 indicates the end of a clause
+                            if (numi == 0) {
+                                // add everything into a clause
+                                Literal[] clauseLiterals = literals.toArray(new Literal[literals.size()]);
+                                Clause newClause = makeCl(clauseLiterals);
+                                clauses.add(newClause);
+                                literals.clear();
 
-                    // for each element in the array, create a matching literal to add into the clause
-                    for (String i : newSplitLine) {
-                        int numi = Integer.parseInt(i);
-                        // numi == 0 indicates the end of a clause
-                        if (numi == 0) {
-                            // add everything into a clause
-                            Literal[] clauseLiterals = literals.toArray(new Literal[literals.size()]);
-                            Clause newClause = makeCl(clauseLiterals);
-                            clauses.add(newClause);
-                            literals.clear();
+                            } else if (numi > 0) {
+                                // numi is positive literal
+                                Literal lit = PosLiteral.make(String.valueOf(numi));
+                                literals.add(lit);
+                            } else {
+                                // numi is negative literal
+                                int positiveValue = numi * -1;
+                                Literal posLit = PosLiteral.make(String.valueOf(positiveValue));
+                                Literal lit = posLit.getNegation();
+                                literals.add(lit);
+                            }
 
-                        } else if (numi > 0) {
-                            // numi is positive literal
-                            Literal lit = PosLiteral.make(String.valueOf(numi));
-                            literals.add(lit);
-                        } else {
-                            // numi is negative literal
-                            int positiveValue = numi * -1;
-                            Literal posLit = PosLiteral.make(String.valueOf(positiveValue));
-                            Literal lit = posLit.getNegation();
-                            literals.add(lit);
+
                         }
-
-
                     }
+                } // finish the while section
+
+                ////////////////////////////////////////////////////////////////////////////////////////
+
+                // PART 2: PUT FORMULA INTO SOLVER (copy paste code from handout)
+
+                // combine clauses into a single formula
+                Clause[] formulaClauses = clauses.toArray(new Clause[clauses.size()]);
+                Formula f2 = makeFm(formulaClauses);
+
+                System.out.println("SAT solver starts!!!");
+                long started = System.nanoTime();
+                Environment e = SATSolver.solve(f2);
+                long time = System.nanoTime();
+                long timeTaken = time - started;
+                System.out.println("Time:" + timeTaken / 1000000.0 + "ms");
+
+                ////////////////////////////////////////////////////////////////////////////////////////
+
+                // PART 3: WRITE OUTPUT TO NEW FILE
+                // PrintWriter writer = new PrintWriter("outputfile.txt");
+                PrintWriter writer = new PrintWriter(args[1]);
+                writer.println("Time:" + timeTaken / 1000000.0 + "ms");
+                PrintWriter writer = new PrintWriter(args[1]);
+                if (e == null) {
+                    writer.println("not satisfiable");
+                } else {
+                    writer.println("satisfiable");
+                    String envString = e.toString();
+                    envString = envString.substring(13, envString.length() - 1);
+                    envString = envString.replaceAll(", ", "\n");
+                    writer.println(envString);
                 }
-            } // finish the while section
+                writer.close();
+                System.out.println("EVERYTHING HAS BEEN DONE SUCCESSFULLY");
 
-            ////////////////////////////////////////////////////////////////////////////////////////
-
-            // PART 2: PUT FORMULA INTO SOLVER (copy paste code from handout)
-
-            // combine clauses into a single formula
-            Clause[] formulaClauses = clauses.toArray(new Clause[clauses.size()]);
-            Formula f2 = makeFm(formulaClauses);
-
-            System.out.println("SAT solver starts!!!");
-            long started = System.nanoTime();
-            Environment e = SATSolver.solve(f2);
-            long time = System.nanoTime();
-            long timeTaken = time - started;
-            System.out.println("Time:" + timeTaken / 1000000.0 + "ms");
-
-            ////////////////////////////////////////////////////////////////////////////////////////
-
-            // PART 3: WRITE OUTPUT TO NEW FILE
-//          PrintWriter writer = new PrintWriter(args[1]);
-            PrintWriter writer = new PrintWriter("outputfile.txt");
-            writer.println("Time:" + timeTaken / 1000000.0 + "ms");
-            if (e == null) {
-                writer.println("not satisfiable");
-            } else {
-                writer.println("satisfiable");
-                String envString = e.toString();
-                envString = envString.substring(13, envString.length() - 1);
-                envString = envString.replaceAll(", ", "\n");
-                writer.println(envString);
+            } catch (IOException e) {
+                System.out.println(e);
             }
-            writer.close();
-            System.out.println("EVERYTHING HAS BEEN DONE SUCCESSFULLY");
 
-        } catch (IOException e) {
-            System.out.println(e);
         }
-
     }
 
 
